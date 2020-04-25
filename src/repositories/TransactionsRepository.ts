703b5aa25fa593c13a341823c38976de9d9f9e0a
Maurício de Sousa Carvalho <mauriciopeba1234@gmail.com>
2020-04-25 18:12:02 -0300

    challenge node finished

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,10 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+}
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +19,29 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    let balance = this.transactions.reduce(
+      (acumulator, { type, value }) => ({
+        ...acumulator,
+        [type]: acumulator[type] + value,
+      }),
+      { income: 0, outcome: 0, total: 0 },
+    );
+
+    balance = { ...balance, total: balance.income - balance.outcome };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, type, value });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
